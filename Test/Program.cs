42dd475b6321c0016ce284dadf29ce95a359e2a6
Alyona <mirka-sobaka@inbox.ru>
2022-11-08 15:30:58 +0400

    добавила задачу без форматирования

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,64 +1,46 @@
 ﻿// // написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше 
 //либо равна 3 символа. Перв массив м ввести с клавиатуры или задать самост на старте алгоритма
-
-// написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше 
-//либо равна 3 символа. Перв массив м ввести с клавиатуры или задать самост на старте алгоритма
-
-
-
-
-//     Console.WriteLine("Введите слова или символы через пробел");
-//     int size = 8;
-//     string[] symbols = new string[size];
-//     FillArray(string [] symbols);
-
-// void FillArray(string [] symb)
-// {
-//     for (int i =0; i< size; i++)
-//     {
-//         symb[i] = Console.ReadLine();
-//     }
-// }
-
-
-//     string[] symbols2 = new string[3];
-//     int count = 0;
-//     PrintArray(symbols); 
     
-//     PrintArray(symbols2); 
-
-
-// void PrintArray(string[]symbols)
-// {
-//     Console.WriteLine("Вывод массива");
-//     for (int i =0; i<symbols.Length; i++)
-//     {
-//         Console.Write(symbols[i] + " ");
-//     }
-//     Console.WriteLine();
-// }
-
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+    int size = 6;
+    string[] symbols = new string[size];
+    string[] symbols2 = new string[symbols.Length];
+    
+    Console.WriteLine($"Введите набор символов {size} раз через Enter, без пробелов");
+    FillArray(symbols);
+    PrintArray(symbols);
+    ArrayFilter(symbols, symbols2);
+    PrintArray(symbols2); 
+    void ArrayFilter(string[] symbols, string[] symbols2)
 {
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < symbols.Length; i++)
     {
-    if(array1[i].Length <= 3)
+    if(symbols[i].Length <= 3)
         {
-        array2[count] = array1[i];
+        symbols2[count] = symbols[i];
         count++;
         }
     }
 }
-void PrintArray(string[] array)
+
+   void FillArray(string [] symbols)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i =0; i< size; i++)
     {
-        Console.Write($"{array[i]} ");
+        symbols[i] = Console.ReadLine();
+    }
+}
+
+   
+
+
+void PrintArray(string[]symbols)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i =0; i<symbols.Length; i++)
+    {
+        Console.Write(symbols[i] + " ");
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
+
